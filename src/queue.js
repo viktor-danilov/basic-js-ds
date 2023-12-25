af04ec682445d7efefa94ed1998b612bc9ef21a2
Viktor Danilov <vikdanilo@yandex.ru>
2023-12-25 17:46:50 +0300

    feat: add queue 2 metods

--- src/queue.js
+++ src/queue.js
@@ -14,20 +14,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
+  constructor() {
+    this.Queue = new Array;
+  }
 
   getUnderlyingList() {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    this.Queue.push(value);
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.Queue.shift();
   }
 }
 
